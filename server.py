773dc984298e7806cab58e37861f62011ec4ba49
Timothy Liu <tlkh.xms@gmail.com>
2019-09-30 15:58:17 +0800

    Update IP address

--- server.py
+++ server.py
@@ -268,4 +268,4 @@ def get_about():
 # if file was executed by itself, start the server process
 if __name__ == "__main__":
     print(" * [i] Starting Flask server")
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
